c52d258f8d464d335b6d2cd14dd9423509c2a9d1
Ashish-619 <69628496+Ashish-619@users.noreply.github.com>
2024-09-01 00:10:28 +0530

    Model Completed

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const Model = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [showModel, setShowModel] = React.useState(false);
+
+    function handleClick() {
+        setShowModel(!showModel);
+    }
+    return (
+        <div className='flex items-center justify-center border flex-col p-5'>
+            <h1 className='text-green-500 mb-5'>Toggle Model</h1>
+            <button className='mb-5' onClick={handleClick}>{showModel ? 'On' : 'Off'}</button>
+            {showModel && <p className='p-2 bg-slate-100 rounded-full text-slate-950 font-bold'>This is a Model</p>}
+        </div>
+    )
 }
 
 export default Model
